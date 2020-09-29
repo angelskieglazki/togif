6afa911464db93e0e44153230af19e190571745b
max <mpano91@gmail.com>
2020-09-29 11:15:53 +0300

    build fix

--- src/video2gif.cpp
+++ src/video2gif.cpp
@@ -26,15 +26,15 @@ private:
 void Video::extract_frames(){
   try{
     VideoCapture cap(this->video_name);
-    if (!cap.isOpened()) CV_Error(CV_StsError, "Can't open video file");
+    if (!cap.isOpened()) CV_Error( cv::Error::StsError , "Can't open video file");
     double fIdx = 0;
-    double frnb(cap.get(CV_CAP_PROP_FRAME_COUNT));
-   // std::cout << "frame count = " << frnb<< std::endl;
+    double frnb(cap.get(cv::CAP_PROP_FRAME_COUNT));
+    std::cout << "frame count = " << frnb<< std::endl;
     for (;;){
-     // std::cout<<"frame : "<<fIdx<<std::endl;
+      std::cout<<"frame : "<<fIdx<<std::endl;
       Mat frame;
       if (fIdx < 0 || fIdx >= frnb) break;
-      cap.set(CV_CAP_PROP_POS_FRAMES, fIdx);
+      cap.set(cv::CAP_PROP_POS_FRAMES, fIdx);
       bool success = cap.read(frame);
       if (success) { this->frames.push_back(frame); fIdx = fIdx + 10;}
       else break;
@@ -49,7 +49,7 @@ void Video::extract_frames(){
 inline Magick::Image Video::mat_2_magick(cv::Mat& src) {
   Magick::Image mgk(src.cols, src.rows, "BGR", Magick::CharPixel, (char *)src.data);
   mgk.compressType(JPEGCompression);
-  mgk.quality(50);
+  mgk.quality(5);
   return mgk;
 }
 
